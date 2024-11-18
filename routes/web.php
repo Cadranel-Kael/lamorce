dbd001b17c78f9375fea82e8c21414ac01eb8da9
kael-cadranel <kael.cadranel00@gmail.com>
2024-11-18 15:10:36 +0100

    Add routes for finances Pages

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::get('finances/overview', [\App\Http\Controllers\FinancesController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('finances.overview');
+
+Route::resource('finances/collections', \App\Http\Controllers\CollectionController::class)
+    ->middleware(['auth']);
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
