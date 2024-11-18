5687174d991883998d042f8615013c7f5f65d68d
kael-cadranel <kael.cadranel00@gmail.com>
2024-11-18 15:11:53 +0100

    Create Transaction factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Collection;
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class TransactionFactory extends Factory
+{
+    protected $model = Transaction::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'amount' => $this->faker->randomFloat(2, 0, 1000),
+            'date_time' => $this->faker->dateTime(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+
+            'collection_id' => Collection::factory(),
+        ];
+    }
+}
