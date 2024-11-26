1a47a9e4b259586e8d38e9f1dc347d5d9bae40a8
kael-cadranel <kael.cadranel00@gmail.com>
2024-11-26 16:36:37 +0100

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
+use App\Models\Collection;
+use App\Models\CollectionType;
+use App\Models\Detente;
+use App\Models\Transaction;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +19,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        CollectionType::factory()
+            ->has(
+                Collection::factory()
+                    ->count(4)
+                    ->has(Transaction::factory()->count(5))
+            )
+            ->create([
+                'name' => 'Base collection',
+            ]);
+
+        CollectionType::factory()
+            ->has(
+                Collection::factory()
+                    ->count(6)
+                    ->has(Transaction::factory()->count(5))
+            )
+            ->create([
+                'name' => 'Project collection',
+            ]);
+
+        Detente::factory(10)->create();
+
+
+        Account::factory(1)->create();
     }
 }
