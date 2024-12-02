a15257c641550b177aada4eb40f02efcc8df78b8
Kael <kael.cadranel00@gmail.com>
2024-12-02 07:49:45 +0100

    Create top nav
    Fix #2

--- resources/views/components/link-nav.blade.php
+++ resources/views/components/link-nav.blade.php
@@ -1,10 +1,14 @@
-<li class="rounded px-4 py-1 {{ request()->routeIs($route) ? 'bg-primary' : '' }}">
+@php
+    $name = $name ?? explode('.', $route)[0];
+    $currentSection = $currentSection ?? explode('.', request()->route()->getName())[0];
+@endphp
+<li class="rounded px-4 py-1 w-44 flex flex-col gap-2 hover:bg-primary {{ $currentSection === $name ? 'bg-primary' : '' }}">
     <a class="font-bold" href="{{ route($route) }}" wire:navigate>
         {{ $slot }}
     </a>
-    @if(request()->routeIs($route))
-        <ul>
-            {{ $linkNav ?? '' }}
+    @if(isset($linkNav) && $currentSection === $name)
+        <ul class="flex-col flex gap-1">
+            {{ $linkNav }}
         </ul>
     @endif
 </li>
--- resources/views/livewire/nav-top.blade.php
+++ resources/views/livewire/nav-top.blade.php
@@ -1,17 +1,22 @@
-<nav class="bg-white shadow py-5 px-8">
-    <div class="max-w-7xl flex items-center gap-4">
+<nav class="z-10 fixed w-screen bg-white shadow py-5 px-8">
+    <div class="ml-72 max-w-6xl justify-between flex items-center gap-4">
         <h2 class="sr-only">{{ __('Navigation') }}</h2>
-        <input type="text" class="border-none rounded w-full" placeholder="{{ __('Search') }}...">
-        <div
-            x-data="{ open: false }"
-            x-on:keydown.escape="open = false"
-            x-on:click.away="open = false"
-            class="relative min-w-fit">
-            <button @click="open = !open" >{{ $user->name }}</button>
-            <ul x-show="open" class="z-10 absolute shadow right-0 p-4 flex flex-col gap-2 top-full bg-white rounded">
-                <li><a href="#" class="block hover:bg-gray-100 rounded p-1">{{ __('Profile') }}</a></li>
-                <li><button wire:click="logout" class="block hover:bg-gray-100 rounded p-1">{{ __('Log Out') }}</button></li>
-            </ul>
+        <div class="flex justify-center relative items-center">
+            <x-icon.magnifying-glass class="text-gray-400 absolute left-0 size-5"/>
+            <input type="text" class="border-none rounded w-full h-full pl-8" placeholder="{{ __('Search') }}...">
         </div>
+        <x-drop-down>
+            <x-drop-down.button>
+                {{ $user->name }}
+            </x-drop-down.button>
+            <x-drop-down.items>
+                <x-drop-down.item>
+                    <a href="#">{{ __('Profile') }}</a>
+                </x-drop-down.item>
+                <x-drop-down.item>
+                    <button wire:click="logout">{{ __('Log Out') }}</button>
+                </x-drop-down.item>
+            </x-drop-down.items>
+        </x-drop-down>
     </div>
 </nav>
