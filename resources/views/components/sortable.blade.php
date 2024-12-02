715acd89ce9d6cd9412d581a3e4522558bba9a39
Kael <kael.cadranel00@gmail.com>
2024-12-02 07:48:53 +0100

    Create sortable component

---
+++ resources/views/components/sortable.blade.php
@@ -0,0 +1,21 @@
+@props(['column', 'sortCol', 'sortAsc'])
+<button
+        type="button"
+        class="group w-full h-full gap-1 flex items-center text-left"
+        wire:click="sortBy('{{ $column }}')"
+>
+    {{ $slot }}
+    @if($sortCol === $column)
+        <div class="text-gray-400">
+            @if($sortAsc)
+                <x-icon.mini-arrow-up class="size-4"/>
+            @else
+                <x-icon.mini-arrow-down class="size-4"/>
+            @endif
+        </div>
+    @else
+        <div class="text-gray-400 opacity-0 group-hover:opacity-100">
+            <x-icon.mini-arrows-up-down class="size-4"/>
+        </div>
+    @endif
+</button>
