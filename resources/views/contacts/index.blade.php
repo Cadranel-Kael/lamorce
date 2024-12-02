d5c85601007bf6805068ac0f484700aee32ad448
Kael <kael.cadranel00@gmail.com>
2024-12-02 07:51:38 +0100

    Create livewire contact table

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,19 +1,6 @@
 <x-app-layout>
-    <h1>Contacts</h1>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Location</th>
-        </tr>
-        @foreach($contacts as $contact)
-            <tr>
-                <td>{{ $contact->name }}</td>
-                <td>{{ $contact->email }}</td>
-                <td>{{ $contact->phone }}</td>
-                <td>{{ $contact->address->country }}</td>
-            </tr>
-        @endforeach
-    </table>
+    <x-slot name="title">
+        {{ __('Contacts') }}
+    </x-slot>
+    <livewire:contact.index.table lazy/>
 </x-app-layout>
