ac8d8e9feac3fe18737cb31f8dcb7902b5066417
Kael <kael.cadranel00@gmail.com>
2024-12-02 07:42:12 +0100

    Link user and contact
    Users now have multiple contact

--- database/migrations/2024_11_28_143113_create_contacts_table.php
+++ database/migrations/2024_11_28_143113_create_contacts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->string('phone')->nullable();
             $table->string('bank_account')->nullable();
             $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
