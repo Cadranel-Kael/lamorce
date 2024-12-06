613c82b974ebb620f795efe4e8e608943ab1fd8d
Kael <kael.cadranel00@gmail.com>
2024-12-06 09:17:02 +0100

    Add actions

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -2,5 +2,18 @@
     <x-slot name="title">
         {{ __('Contacts') }}
     </x-slot>
+    <x-slot name="actions">
+        <x-button
+            x-data
+            @click="$dispatch('openModal', {
+            title: '{{ __('Create Contact') }}',
+            component: 'contact.edit.form'
+        })"
+        >
+            {{ __('Create Contact') }}
+        </x-button>
+    </x-slot>
+
+
     <livewire:contact.index.table lazy/>
 </x-app-layout>
