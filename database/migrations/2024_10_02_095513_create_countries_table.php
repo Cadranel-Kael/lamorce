ac6da1d9c4122ee1f81eae73ffc220dc53717dd7
Kael <kael.cadranel00@gmail.com>
2024-12-06 09:10:39 +0100

    Remove unused iso 3 code

--- database/migrations/2024_10_02_095513_create_countries_table.php
+++ database/migrations/2024_10_02_095513_create_countries_table.php
@@ -10,7 +10,6 @@ return new class extends Migration {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('code');
-            $table->string('iso3_code');
             $table->string('flag_url');
             $table->timestamps();
             $table->softDeletes();
