4a3959998ef5479d8c3f50f96a630c30a4803974
Kael <kael.cadranel00@gmail.com>
2024-12-06 09:07:32 +0100

    Separate name into first & last

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -12,11 +13,22 @@ class Contact extends Model
     use SoftDeletes, HasFactory;
 
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'phone',
         'bank_account',
+        'address_id',
     ];
+
+    protected $appends = ['name'];
+
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->first_name . ' ' . $this->last_name
+        );
+    }
 
     public function address(): HasOne
     {
