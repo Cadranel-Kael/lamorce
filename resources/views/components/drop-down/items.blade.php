b489311d6cd70b663f8cc2cfc00b5b065c09c570
Kael <kael.cadranel00@gmail.com>
2024-12-06 09:13:35 +0100

    Fix blinking issue when loading

--- resources/views/components/drop-down/items.blade.php
+++ resources/views/components/drop-down/items.blade.php
@@ -1,3 +1,3 @@
-<ul x-show="open" class="z-10 absolute shadow right-0 p-4 flex flex-col gap-2 top-full bg-white rounded">
+<ul x-show="open" x-cloak class="z-10 absolute shadow right-0 p-4 flex flex-col gap-2 top-full bg-white rounded">
     {{ $slot }}
 </ul>
