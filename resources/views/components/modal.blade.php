06bfdfdd94724d77035638e388ff2201b0078b7f
Kael <kael.cadranel00@gmail.com>
2024-12-09 16:09:44 +0100

    Add modal type

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,12 +1,30 @@
-<div class="absolute top-0 left-0 right-0 bottom-0 justify-center items-center flex">
+@props([
+    'component',
+    'data' => [],
+    'title',
+    'index' => 0,
+])
+<div
+    x-ref="modal"
+    x-trap.inert.noScroll="true"
+    x-data="modal({
+        index: {{ $index }}
+    })"
+    @keydown.window.escape="close()"
+
+    aria-labelledby="slide-over-title"
+    role="dialog"
+    aria-modal="true"
+    class="relative">
     <div
-        class="inset-0 fixed w-full z-10 bg-black opacity-10 h-full"
+        @click="close()"
+        class="inset-0 fixed z-10 bg-black opacity-10"
         aria-hidden="true"></div>
-    <div class="bg-white z-10 rounded">
-        <div>
+    <div class="fixed overflow-hidden z-20 inset-0 flex pointer-events-none justify-center items-center">
+        <div class="bg-white rounded max-w-4xl pointer-events-auto w-full">
             <div class="flex justify-between shadow rounded-t items-center px-8 py-4 bg-white">
-                <div class="font-bold">{{ $modalTitle }}</div>
-                <button @click="open = false" title="{{ __('Close the drawer') }}">
+                <div class="font-bold">{{ $title }}</div>
+                <button type="button" @click="close()" title="{{ __('Close the drawer') }}">
                     <svg class="h-6 w-6 text-gray-400 hover:text-black" title="{{ __('close') }}"
                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
@@ -15,9 +33,31 @@
                     </svg>
                 </button>
             </div>
-        </div>
-        <div class="p-8">
-            {{ $slot }}
+            <div class="px-8 py-4">
+                @livewire($component, $data, key($component))
+            </div>
         </div>
     </div>
 </div>
+@script
+<script>
+    Alpine.data('modal', (config) => {
+        return {
+            open: true,
+            index: config.index,
+
+            close() {
+                this.open = false;
+            },
+
+            init() {
+                this.$watch('open', value => {
+                    if (!value) {
+                        this.$wire.closeModal(this.index);
+                    }
+                });
+            }
+        }
+    });
+</script>
+@endscript
