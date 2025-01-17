e0dd000fc5ec51973391289ee9c9bf82b64df2ec
Kael <kael.cadranel00@gmail.com>
2025-01-17 08:53:43 +0100

    Change name

--- database/factories/MandateFactory.php
+++ database/factories/MandateFactory.php
@@ -2,18 +2,18 @@
 
 namespace Database\Factories;
 
-use App\Models\Detente;
+use App\Models\Mandate;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
-class DetenteFactory extends Factory
+class MandateFactory extends Factory
 {
-    protected $model = Detente::class;
+    protected $model = Mandate::class;
 
     public function definition(): array
     {
         return [
-            'date_time' => $this->faker->dateTimeBetween('-1 year', '+1 year'),
+            'date_time' => $this->faker->dateTimeBetween('-1 year'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
